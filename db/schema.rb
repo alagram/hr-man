23e39ea2178daced13859b07bf979038664cf0f3
Albert Agram <albert.agram@gmail.com>
2014-11-04 15:26:03 +0000

    change emp_id to string in emergencies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102162731) do
+ActiveRecord::Schema.define(version: 20141104064126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20141102162731) do
   end
 
   create_table "emergencies", force: true do |t|
-    t.integer  "emp_id"
+    t.string   "emp_id"
     t.string   "title"
     t.string   "first_name"
     t.string   "other_names"
