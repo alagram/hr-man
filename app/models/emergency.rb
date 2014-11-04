eb461fcc4e1e002f8e13ce3b569754c471f2fe5d
Albert Agram <albert.agram@gmail.com>
2014-11-04 15:37:55 +0000

    initial emergency class

---
+++ app/models/emergency.rb
@@ -0,0 +1,11 @@
+class Emergency < ActiveRecord::Base
+  belongs_to :employee, foreign_key: 'emp_id', primary_key: 'emp_id'
+
+  def to_s
+    if other_names.present?
+      "#{last_name}, #{other_names} #{first_name}"
+    else
+      "#{last_name}, #{first_name}"
+    end
+  end
+end
