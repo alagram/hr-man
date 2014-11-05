c50ba7151496abc97d64a2346857be12879fc3d6
Albert Agram <albert.agram@gmail.com>
2014-11-05 09:32:20 +0000

    fix failing tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -6,16 +6,13 @@ RSpec.describe Employee, :type => :model do
  it { should validate_presence_of(:last_name) }
  it { should validate_presence_of(:dob) }
  it { should validate_presence_of(:gender_id) }
+ it { should have_many(:emergencies).with_foreign_key('emp_id') }
 
  describe "search" do
   it "returns an array if one record is found" do
-    jane = Fabricate(:employee, emp_id: 1, first_name: "Jane", last_name: "Smith", other_names: "Abena")
-    expect(Employee.search("Jane")).to eq([jane])
-  end
-  it "searches first_name, last_name, other_names, emp_id columns and returns an array if result is found" do
-    jane = Fabricate(:employee, emp_id: 1, first_name: "Jane", last_name: "Smith", other_names: "Abena")
-    bob = Fabricate(:employee, emp_id: 2, first_name: "Bob", last_name: "Doe", other_names: "Kofi")
-    expect(Employee.search("Doe")).to eq([bob])
+    jane = Fabricate(:employee, emp_id: "E1", first_name: "Jane", last_name: "Smith", other_names: "Abena")
+    bob = Fabricate(:employee, emp_id: "E2", first_name: "Bob", last_name: "Doe")
+    expect(Employee.search("Jane Abena Smith")).to eq([jane])
   end
   it "returns an array if search for emp id is separated by commas" do
     jane = Fabricate(:employee, emp_id: "emp_1", first_name: "Jane", last_name: "Smith", other_names: "Abena")
