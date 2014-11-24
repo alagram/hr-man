b48968ea0729d3a2256383648debc2ea51e34d50
Albert Agram <albert.agram@gmail.com>
2014-11-24 15:03:14 +0000

    add welcome_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'employee_managers', to: 'employees#employee_managers'
   get 'sign_in', to: 'sessions#new'
   get 'sign_out', to: 'sessions#destroy'
+  get 'welcome', to: 'homes#welcome'
   resources :employees, except: :destroy do
     collection do
       get :search
