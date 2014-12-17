d1b84c79f42572a63522f48e36d931a157ec7c3a
Albert Agram <albert.agram@gmail.com>
2014-12-17 07:25:24 +0000

    fix typos

--- app/assets/javascripts/find_end_date.js
+++ app/assets/javascripts/find_end_date.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $("#get-end-date").click(function(event){
     event.preventDefault();
-    var date_enetred = $("#leave_request_date_from").val();
+    var date_enetered = $("#leave_request_date_from").val();
     var num_days = $("#leave_request_num_of_days").val();
     $.ajax({
       url: '/leave_requests/get_end_date',
       dataType: 'script',
-      data: { date: date_enetred, days: num_days },
+      data: { date: date_enetered, days: num_days },
     })
   })
 });
