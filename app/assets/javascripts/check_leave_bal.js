33a1141f350772a5b80e5522049efaac5ea26fd6
Albert Agram <albert.agram@gmail.com>
2014-12-24 11:01:28 +0000

    update check_leave_bal script

--- app/assets/javascripts/check_leave_bal.js
+++ app/assets/javascripts/check_leave_bal.js
@@ -1,23 +1,29 @@
+"use strict";
 $(window).load(function(){
-  $("#get-end-date").attr('disabled', 'disabled');
+  $("#get-end-date").prop('disabled', true);
 })
 
 $(document).ready(function(){
+  keyUpAjax();
+});
+
+function keyUpAjax (){
   $("#leave_request_num_of_days").keyup(function(){
     var days = $("#leave_request_num_of_days").val();
-    var option_text = $("#leave_request_leave_type_id :selected").val();
-    var full_or_half = $("input[name='leave_request[day_type]']:checked", "#new_leave_request").val();
+    var optionText = $("#leave_request_leave_type_id :selected").val();
+    var dayTypeRadionBtn = $("input[name='leave_request[day_type]']:checked", "#new_leave_request").val();
 
     if (days === "") {
-      $("#get-end-date").attr('disabled', 'disabled');
+      $("#get-end-date").prop('disabled', true);
     }
 
     if (parseInt(days) % 1 === 0){
       $.ajax({
         url:'/leave_requests/check_leave_bal',
+        type: 'POST',
         dataType: 'script',
-        data: { num_days: days, leave_option: option_text, day_type: full_or_half },
+        data: { num_days: days, leave_option: optionText, day_type: dayTypeRadionBtn },
       })
     }
   })
-});
+}
